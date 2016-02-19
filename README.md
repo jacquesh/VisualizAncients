2ad1bb426600bfc0e2319d931dc755d619cb3d39
Jacques Heunis <jaaksemail@gmail.com>
2016-02-19 13:18:31 +0200

    Add some actually possibly useful parsing of the data, along with a readme for further reading

---
+++ README.md
@@ -0,0 +1,7 @@
+Info sources:
+- https://github.com/skadistats/clarity
+- https://github.com/skadistats/clarity/issues/18
+- https://github.com/skadistats/skadi/wiki/Hero-Names-and-IDs
+- https://github.com/dschleck/edith
+- https://andersdrachen.files.wordpress.com/2014/07/ieee-gem2014_submission_72.pdf
+- http://www.lighti.de/projects/dotalys-2/
--- Reparser.java
+++ Reparser.java
@@ -1,12 +1,72 @@
 import skadistats.clarity.Clarity;
-import skadistats.clarity.wire.common.proto.Demo.CDemoFileInfo;
+import skadistats.clarity.source.MappedFileSource;
+
+import skadistats.clarity.processor.runner.Context;
+import skadistats.clarity.processor.runner.SimpleRunner;
+
+import skadistats.clarity.processor.gameevents.OnGameEvent;
+import skadistats.clarity.processor.reader.OnTickStart;
+import skadistats.clarity.processor.entities.OnEntityCreated;
+import skadistats.clarity.processor.entities.OnEntityUpdated;
+import skadistats.clarity.processor.entities.OnEntityDeleted;
+import skadistats.clarity.model.GameEvent;
+import skadistats.clarity.model.Entity;
+import skadistats.clarity.model.FieldPath;
 
 public class Reparser
 {
+    private boolean hasPrinted = false;
+
+    @OnGameEvent
+    public void onGameEvent(Context ctx, GameEvent evt)
+    {
+        //System.out.println(evt.toString());
+    }
+
+    @OnTickStart
+    public void onTickStart(Context ctx, boolean something)
+    {
+    }
+
+    /*
+    @OnEntityCreated
+    public void onEntityCreated(Context ctx, Entity ent)
+    {
+        if(!hasPrinted)
+        {
+            System.out.printf("Created entity %s\n", ent.toString());
+            hasPrinted = true;
+        }
+    }*/
+
+    @OnEntityUpdated
+    public void onEntityUpdated(Context ctx, Entity ent, FieldPath[] something, int somethingElse)
+    {
+        String entName = ent.getDtClass().getDtName();
+        System.out.printf("Entity %s updated\n", entName);
+        if(entName.equals("CDOTA_Unit_Hero_Lion"))
+        {
+            System.out.println(ent.toString());
+        }
+    }
+
+    /*
+    @OnEntityDeleted
+    public void onEntityDeleted(Context ctx, Entity ent)
+    {
+        //System.out.printf("Deleted entity %d\n", ent.getSerial());
+    }*/
+
+
     public static void main(String[] args) throws Exception
     {
+        long tStart = System.currentTimeMillis();
         String inputFile = "test.dem";
-        CDemoFileInfo info = Clarity.infoForFile(inputFile);
-        System.out.println(info);
+        MappedFileSource source = new MappedFileSource(inputFile);
+        SimpleRunner runner = new SimpleRunner(source);
+        Reparser parser = new Reparser();
+        runner.runWith(parser);
+        long tEnd = System.currentTimeMillis();
+        System.out.printf("Total time taken: %f", (tEnd-tStart)/1000.0f);
     }
 }
