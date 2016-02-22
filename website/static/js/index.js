7465ee0c601d882e5d3b335ef5b3c6015e11d415
Jethro Muller <JethroMuller@gmail.com>
2016-02-22 23:08:40 +0200

    Add basic visualization test.
    * Add jQuery.
    * Add jQueryUI.
    * Add jCanvas.
    * Add Dota 2 map from yasp.co.

---
+++ website/static/js/index.js
@@ -0,0 +1,48 @@
+(function ($) {
+  'use strict';
+
+  var mapManager = {
+    $map: $('#dota-map'),
+
+    drawMapPoint: function(x, y, colour) {
+      // * 3 because the map is smaller than canvas
+      x = (x - 60) * 3;
+      y = 420 - ((y - 50) * 3);
+      this.$map.drawArc({
+        fillStyle: '#FFF',
+        x: x, y: y,
+        radius: 8
+      }).drawArc({
+        fillStyle: colour,
+        x: x, y: y,
+        radius: 6
+      });
+    },
+
+    resetMap: function() {
+      this.$map.clearCanvas();
+    }
+  };
+
+  var setupPlayerData = function () {
+    var $timeSlider = $("#time-slider");
+
+    $timeSlider.slider({
+      value: 0,
+      min: 0,
+      max: playerData.length - 1,
+      step: 1,
+      slide: function(event, ui) {
+        $("#amount").text("Time: " + ui.value );
+        var points = playerData[ui.value];
+        mapManager.resetMap();
+        mapManager.drawMapPoint(points.x, points.y, "#000");
+      }
+    });
+    //$timeSlider.slider('option', 'slide').call($timeSlider);
+    $("#amount").text("Time: " + $timeSlider.slider("value"));
+  };
+
+  $(document).ready(setupPlayerData);
+
+})(jQuery);
