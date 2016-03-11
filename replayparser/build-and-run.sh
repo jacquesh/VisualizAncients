a76874171c1f123136f705336b950457133615ef
Jethro Muller <JethroMuller@gmail.com>
2016-03-11 15:54:29 +0200

    Add cli argument support to specify replay.

--- replayparser/build-and-run.sh
+++ replayparser/build-and-run.sh
@@ -1,2 +1,2 @@
 javac -implicit:class -cp .:./deps/* -d ./classfiles Main.java;
-java -Dlogback.statusListenerClass=ch.qos.logback.core.status.NopStatusListener -cp ./classfiles:./deps/* Main
+java -Dlogback.statusListenerClass=ch.qos.logback.core.status.NopStatusListener -cp ./classfiles:./deps/* Main $1
