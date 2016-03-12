1632f71faa543228377c82bbc5a48551601eebf9
Alan Berman <brmala003@nigeria.cs.uct.ac.za>
2016-03-12 19:24:49 +0200

    little graphs done

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -59,7 +59,8 @@
     var dataCharArr = inflater.result;
     var dataStr = charArr2Str(dataCharArr);
     replayData = JSON.parse(dataStr);
-
+	
+	
     var $timeSlider = $('#time-slider');
     $timeSlider.slider({
       value: 0,
@@ -97,15 +98,16 @@
 
   var loadPlayerData = function() {
     var req = new XMLHttpRequest();
-    req.open("GET", "/static/data.zjson", true);
+    req.open("GET", "../static/js/data.zjson", true);
     req.responseType = "arraybuffer";
     req.onload = function(event) {
         var bytes = new Uint8Array(req.response);
         setupPlayerData(bytes);
+        
     };
     req.send();
   };
 
+  
   $(document).ready(loadPlayerData);
-
 })(jQuery);
